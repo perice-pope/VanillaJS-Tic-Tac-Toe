9fe424ae7bc70e649c6ad9641e92863d4b911929
Perice Pope <perice09@gmail.com>
2021-10-18 16:57:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// MODEL 
+// consts - to lookup data structures
+// state variables 
+
+// VIEW 
+// cache dom elements 
+
+
+
+// connect to view 
+// Event listeners
+
+
+
+// Controller - functions 
